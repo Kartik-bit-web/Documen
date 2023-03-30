6bd5d759f447d1539e43fdbe814b1203112df038
kartik <km8469879@gmail.com>
2023-03-30 18:01:19 +0000

    My first we

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def hel():
+    return render_template('index.html', name=' kartik')
+
+@app.route('/resis')
+def resis():
+    return render_template('resis.html')
+
+@app.route('/hall')
+def hall():
+    return render_template('hall.html')
+
+@app.route('/dated')
+def dated():
+    return render_template('dated.html')
+
+@app.route('/function')
+def fun():
+    return render_template('fun.html')
+
+@app.route('/booking')
+def booking():
+    return render_template('booking.html')
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
+@app.route('/deco')
+def deco():
+    return render_template('deco.html')
+
